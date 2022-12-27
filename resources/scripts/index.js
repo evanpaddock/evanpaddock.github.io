86590aff662ee9bfbb28bc199ab0ff730d9a32f1
evanpaddock <edpaddock@crimson.ua.edu.com>
2022-12-26 23:47:14 -0600

    pagecounter added

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,20 +1,3 @@
-var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
-var visitCount = localStorage.getItem("page_view");
-
-// Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
+function liveViews(response) {
+  document.getElementById('visits').innerText = response.value;
 }
-counterContainer.innerHTML = visitCount;
-
-// Adding onClick event listener
-resetButton.addEventListener("click", () => {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-  counterContainer.innerHTML = visitCount;
-});
